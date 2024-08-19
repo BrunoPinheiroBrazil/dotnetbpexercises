db5acbbb615ef11978f1068236dc4966e57ce375
BrunoPinheiroBrazil <brunopinheirobrazil@gmail.com>
2024-08-19 19:29:04 +0000

    primeiro exercicio amor

---
+++ microsoft.botsay/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Tipos de variáveis: 
+//int: inteiro
+//string: texto
+//char: letra ou caractere
+//float: numeros flutuantes
+//boolean: booleano, verdadeiro ou falso. 
+//Com base nisso escreva quais os tipos de variaveis abaixo nos comentários: 
+
+//Exemplo:
+var a = "Eu sou um texto"; // O tipo de variável é string.
+
+//Exercicios: 
+var b = 100; //
+
+var c = "100"; //
+
+var d = 100.5f; //
+
+var e = false; //
+
+var f = "true"; //
+
+var g = true; // 
+
+var h = 'a'; //
+
+var i = "a"; //
